b47f79539fc10c372958b15a6a989bf14885ea70
Rutharr <arthur.sco45@gmail.com>
2023-04-22 00:20:04 +0200

    Reprise à zéro du projet sur des bases claires ainsi qu'une autre approche, ajout de la distribution de cartes uniques pour le joueur et le croupier dont une caché pour ce dernier

--- script.js
+++ script.js
@@ -1,192 +1,99 @@
-function startGame() {
-    deck = shuffleDeck(createDeck());
-    dealerHand = [deck.pop(), deck.pop()];
-    playerHand = [deck.pop(), deck.pop()];
-    playerScore = calculateHand(playerHand);
-    dealerScore = calculateHand(dealerHand);
-    renderGame();
+const valeursCartes = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'V', 'D', 'R'];
+const couleursCartes = ['coeur', 'pique', 'carreau', 'trefle'];
+const valeursNumeriques = {
+  'A': 11,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  '10': 10,
+  'V': 10,
+  'D': 10,
+  'R': 10,
+};
+let cartesTirees = [];
+
+function masquerBouton() {
+  document.getElementById("bouton-commencer").style.display = "none";
+}
+
+
+function initialiserPartie() {
+    let croupierCartes = [];
+    let joueurCartes = [];
+  
+  let croupierCartesContainer = document.createElement("div");
+  croupierCartesContainer.classList.add("croupier-container");
+  document.body.appendChild(croupierCartesContainer);
+  
+  // Distribue 2 cartes au croupier
+  for (let i = 0; i < 2; i++) {
+    let valeur = choisirCarte();
+    let couleur = choisirCouleur();
+    croupierCartes.push({ valeur: valeur, couleur: couleur });
+
+    let carte = document.createElement("img");
+    carte.classList.add("card");
+    if (i === 0) {
+      carte.src = "cartes/dos-bleu.png"; // Carte face cachée
+    } else {
+      carte.src = "cartes/"+valeur+"-"+couleur+".png";
+    }
+    croupierCartesContainer.appendChild(carte);
+  }
+  let croupierText = document.createElement("h2");
+  croupierText.innerText = "Cartes du croupier";
+  croupierCartesContainer.appendChild(croupierText);
+
+  let joueurCartesContainer = document.createElement("div");
+  joueurCartesContainer.classList.add("joueur-container");
+  document.body.appendChild(joueurCartesContainer);
+
+  // Distribue 2 cartes au joueur
+  for (let i = 0; i < 2; i++) {
+    let valeur = choisirCarte();
+    let couleur = choisirCouleur();
+    joueurCartes.push({ valeur: valeur, couleur: couleur });
+
+    let carte = document.createElement("img");
+    carte.classList.add("card");
+    carte.src = "cartes/"+valeur+"-"+couleur+".png";
+    joueurCartesContainer.appendChild(carte);
   }
 
-  document.addEventListener('DOMContentLoaded', function() {
-    startGame();
-  });
+  let joueurText = document.createElement("h2");
+  joueurText.innerText = "Cartes du joueur";
+  joueurCartesContainer.appendChild(joueurText);
+  
+  console.log("Cartes du croupier : ", croupierCartes);
+  console.log("Cartes du joueur : ", joueurCartes);
+
+}
+
+// Fonction pour choisir une carte aléatoire
+function choisirCarte() {
+  let cartes = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R"];
+  let index = Math.floor(Math.random() * cartes.length);
+  let carte = cartes[index];
+  
+  // Vérifier si la carte a déjà été tirée
+  if (cartesTirees.includes(carte)) {
+    // Si oui, tirer une nouvelle carte
+    return choisirCarte();
+  } else {
+    // Sinon, ajouter la carte aux cartes déjà tirées et la renvoyer
+    cartesTirees.push(carte);
+    return carte;
+  }
+}
 
 
-let deck = [];
-      const suits = ["coeur", "pique", "trefle", "carreau"];
-      const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R","A" ];
-      let dealerHand = [];
-      let playerHand = [];
-      let dealerScore = 0;
-      let playerScore = 0;
-
-      // Functions
-      function createDeck() {
-        deck = [];
-        for (let i = 0; i < suits.length; i++) {
-          for (let j = 0; j < values.length; j++) {
-            let card = {
-              suit: suits[i],
-              value: values[j]
-            };
-            deck.push(card);
-          }
-        }
-        shuffleDeck(deck);
-        return deck;
-      }
-      
-
-      function shuffleDeck(deck) {
-        for (let i = deck.length - 1; i > 0; i--) {
-          let j = Math.floor(Math.random() * (i + 1));
-          let temp = deck[i];
-          deck[i] = deck[j];
-          deck[j] = temp;
-        }
-      }
-
-      function getCardImageUrl(card) {
-        return `cartes/${card.value}-${card.suit}.png`;
-      }
-
-      function dealCards() {
-        dealerHand = [getNextCard(), getNextCard()];
-        playerHand = [getNextCard(), getNextCard()];
-        dealerScore = calculateScore(dealerHand);
-        playerScore = calculateScore(playerHand);
-      }
-      
-      function renderGame() {
-        dealerHandElement.innerHTML = '';
-        playerHandElement.innerHTML = '';
-        
-        // Ajouter les cartes du croupier
-        for (let i = 0; i < dealerHand.length; i++) {
-          let cardElement = document.createElement("img");
-          cardElement.setAttribute("src", getCardImageUrl(dealerHand[i]));
-          cardElement.setAttribute("class", "card");
-          dealerHandElement.appendChild(cardElement);
-        }
-        
-        // Ajouter les cartes du joueur
-        for (let i = 0; i < playerHand.length; i++) {
-          let cardElement = document.createElement("img");
-          cardElement.setAttribute("src", getCardImageUrl(playerHand[i]));
-          cardElement.setAttribute("class", "card");
-          playerHandElement.appendChild(cardElement);
-        }
-        
-        if (playerScore < 21) {
-          hitButton.style.display = "inline-block";
-        }
-        
-        standButton.style.display = "inline-block";
-      }
-      
-      
-      function getNextCard() {
-        return deck.shift();
-      }
-
-      function calculateScore(hand) {
-        let score = 0;
-        let hasAce = false;
-        for (let i = 0; i < hand.length; i++) {
-          let cardValue = hand[i].value;
-          if (cardValue === "A") {
-            hasAce = true;
-          }
-          score += getCardNumericValue(cardValue);
-        }
-        if (hasAce && score + 10 <= 21) {
-          score += 10;
-        }
-        return score;
-      }
-
-      function getCardNumericValue(value) {
-        switch(value) {
-          case "A":
-            return 1;
-          case "02":
-            return 2;
-          case "03":
-            return 3;
-          case "04":
-            return 4;
-          case "05":
-            return 5;
-          case "06":
-            return 6;
-          case "07":
-            return 7;
-          case "08":
-            return 8;
-          case "09":
-            return 9;
-          default:
-            return 10;
-        }
-      }
-
-      function playerTurn() {
-        let card = getNextCard();
-        playerHand.push(card);
-        playerScore = calculateScore(playerHand);
-        renderGame();
-        if (playerScore > 21) {
-          endGame();
-        }
-      }
-
-      function endGame() {
-        if (playerScore > 21) {
-          endMessage("Vous avez dépassé 21. Le croupier a gagné !");
-        } else if (dealerScore > 21) {
-          endMessage("Le croupier a dépassé 21. Vous avez gagné !");
-        } else if (playerScore > dealerScore) {
-          endMessage("Vous avez gagné !");
-        } else if (dealerScore > playerScore) {
-          endMessage("Le croupier a gagné !");
-        } else {
-          endMessage("Match nul !");
-        }
-      
-        dealButton.style.display = "inline-block";
-        hitButton.style.display = "none";
-        standButton.style.display = "none";
-      }
-      
-      function endMessage(message) {
-        let messageElem = document.createElement("div");
-        messageElem.textContent = message;
-        messageElem.style.color = "white";
-        messageElem.style.fontSize = "30px";
-        messageElem.style.fontWeight = "bold";
-        messageElem.style.textAlign = "center";
-        messageElem.style.marginTop = "20px";
-        blackjackTable.appendChild(messageElem);
-      }
-      
-      // Select the elements
-const blackjackTable = document.querySelector('#blackjack-table');
-const dealerHandElement = document.getElementById("dealer-hand");
-const playerHandElement = document.getElementById("player-hand");
-const deckElement = document.querySelector('#deck');
-
-// Create buttons
-const dealButton = document.createElement('button');
-dealButton.textContent = 'Distribuer';
-const hitButton = document.createElement('button');
-hitButton.textContent = 'Carte';
-const standButton = document.createElement('button');
-standButton.textContent = 'Rester';
-
-// Add buttons to the document
-blackjackTable.appendChild(dealerHandElement);
-blackjackTable.appendChild(playerHandElement);
-blackjackTable.appendChild(dealButton);
-blackjackTable.appendChild(hitButton);
-blackjackTable.appendChild(standButton);
-blackjackTable.appendChild(deckElement);
+// Fonction pour choisir une couleur aléatoire
+function choisirCouleur() {
+  let couleurs = ["coeur", "pique", "carreau", "trefle"];
+  return couleurs[Math.floor(Math.random() * couleurs.length)];
+}
