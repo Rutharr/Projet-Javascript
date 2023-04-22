9dceb8008f946df5b22e98233b2a1f39017234e8
Rutharr <arthur.sco45@gmail.com>
2023-04-23 00:24:10 +0200

    Ajout du système de point, de la fonction tirage/ fonction rester/ calcul de fin de partie ( le jeu est fonctionnel à l'heure actuelle)

--- script.js
+++ script.js
@@ -16,16 +16,22 @@ const valeursNumeriques = {
   'R': 10,
 };
 let cartesTirees = [];
+let joueurCartes = [];
+let croupierCartes = [];
 
 function masquerBouton() {
   document.getElementById("bouton-commencer").style.display = "none";
+  document.getElementById("bouton-tirer").style.display = "inline-block";
+  document.getElementById("bouton-rester").style.display = "inline-block";
 }
 
+function recacherBouton(){
+  document.getElementById("bouton-tirer").style.display = "none";
+  document.getElementById("bouton-rester").style.display = "none";
+  document.getElementById("bouton-recommencer").style.display = "inline-block";
+}
 
 function initialiserPartie() {
-    let croupierCartes = [];
-    let joueurCartes = [];
-  
   let croupierCartesContainer = document.createElement("div");
   croupierCartesContainer.classList.add("croupier-container");
   document.body.appendChild(croupierCartesContainer);
@@ -53,6 +59,8 @@ function initialiserPartie() {
   joueurCartesContainer.classList.add("joueur-container");
   document.body.appendChild(joueurCartesContainer);
 
+  let joueurPointsContainer = document.querySelector(".joueur-points");
+
   // Distribue 2 cartes au joueur
   for (let i = 0; i < 2; i++) {
     let valeur = choisirCarte();
@@ -63,7 +71,12 @@ function initialiserPartie() {
     carte.classList.add("card");
     carte.src = "cartes/"+valeur+"-"+couleur+".png";
     joueurCartesContainer.appendChild(carte);
+
+    let totalPoints = calculerPoints(joueurCartes);
+    joueurPointsContainer.innerText = "Points : "+totalPoints;
   }
+
+  
 
   let joueurText = document.createElement("h2");
   joueurText.innerText = "Cartes du joueur";
@@ -72,6 +85,10 @@ function initialiserPartie() {
   console.log("Cartes du croupier : ", croupierCartes);
   console.log("Cartes du joueur : ", joueurCartes);
 
+  let croupierValeurMain = calculerPoints(croupierCartes);
+  let joueurValeurMain = calculerPoints(joueurCartes);
+  console.log("Valeur de la main du croupier : ", croupierValeurMain);
+  console.log("Valeur de la main du joueur : ", joueurValeurMain);
 }
 
 // Fonction pour choisir une carte aléatoire
@@ -96,4 +113,88 @@ function choisirCarte() {
 function choisirCouleur() {
   let couleurs = ["coeur", "pique", "carreau", "trefle"];
   return couleurs[Math.floor(Math.random() * couleurs.length)];
+}
+
+
+function calculerPoints(cartes) {
+  let totalPoints = 0;
+  let nombreAs = 0;
+  
+  for (let i = 0; i < cartes.length; i++) {
+    let valeur = cartes[i].valeur;
+    totalPoints += valeursNumeriques[valeur];
+    
+    if (valeur === "A") {
+      nombreAs++;
+    }
+  }
+  
+  // Traiter les As en tant que 1 si nécessaire
+  while (nombreAs > 0 && totalPoints > 21) {
+    totalPoints -= 10;
+    nombreAs--;
+  }
+  
+  return totalPoints;
+}
+
+function tirerCarte() {
+  let joueurCartesContainer = document.querySelector(".joueur-container");
+  let joueurPointsContainer = document.querySelector(".joueur-points");
+
+  // Si le joueur a déjà perdu, on ne tire plus de carte
+  if (joueurPointsContainer.innerText === "Perdu !") {
+    return;
+  }
+
+  let valeur = choisirCarte();
+  let couleur = choisirCouleur();
+  let nouvelleCarte = { valeur: valeur, couleur: couleur };
+  joueurCartes.push(nouvelleCarte);
+
+  let carte = document.createElement("img");
+  carte.classList.add("card");
+  carte.src = "cartes/"+valeur+"-"+couleur+".png";
+  joueurCartesContainer.appendChild(carte);
+
+  let totalPoints = calculerPoints(joueurCartes);
+  joueurPointsContainer.innerText = "Points : "+totalPoints;
+
+  // Si le joueur a dépassé 21 points, il perd la partie
+  if (totalPoints > 21) {
+    joueurPointsContainer.innerText = "Perdu ! Vous avez dépasser 21";
+    document.getElementById("bouton-tirer").style.display = "none";
+    document.getElementById("bouton-rester").style.display = "none";
+    document.getElementById("bouton-recommencer").style.display = "inline-block";
+  }
+}
+
+function Rester() {
+  // Afficher la première carte du croupier
+  let carteCroupier = document.querySelector(".croupier-container .card:first-child");
+  carteCroupier.src = "cartes/" + croupierCartes[0].valeur + "-" + croupierCartes[0].couleur + ".png";
+
+  let croupierCartesContainer = document.querySelector(".croupier-container");
+  let pointsCroupier = calculerPoints(croupierCartes);
+  let croupierPointsText = document.createElement("h3");
+  croupierPointsText.innerText = "Points du croupier: " + pointsCroupier;
+  croupierCartesContainer.appendChild(croupierPointsText);
+
+  let resultatText = document.createElement("h3");
+
+  if (pointsCroupier > calculerPoints(joueurCartes)){
+    resultatText.innerText = "Vous avez perdu, le croupier a une meilleur main !";
+  }
+  else if(pointsCroupier == calculerPoints(joueurCartes)){
+    resultatText.innerText = "C'est une égalité, aucun gagnant !";
+  }
+  else {
+    resultatText.innerText = "Félicitations vous avez battu le croupier !";
+  }
+
+  document.body.appendChild(resultatText);
+}
+
+function recommencerPartie(){
+  location.reload();
 }
